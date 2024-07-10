c0f443a98a45f55def92bb6ed6e9599cc7fb9a05
Hmad Afzal <hmadafzal00@gmail.com>
2024-07-10 17:24:24 +0500

    env fixed

--- src/components/SingleProduct.tsx
+++ src/components/SingleProduct.tsx
@@ -29,22 +29,13 @@ const SingleProduct = ({ singleProduct }: { singleProduct: any }) => {
                   <div>
                     <h1 className="font-bold text-sm">About this item</h1>
                     <li>
-                      Processor: High performance MediaTek G85 Enhance gaming
-                      with 1GHz GPU 8GB of RAM including 4GB virtual 6.74 HD+
-                      90Hz display with Corning Gorilla Glass 3 Protection 50MP
-                      AI Triple camera Fast Side fingerprint 5000mAh Battery
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quaerat omnis assumenda! Delectus sit inventore unde aliquam commodi asperiores illo necessitatibus! Natus eius dolorum optio mollitia soluta ipsam, odio omnis.
                     </li>
                     <li>
-                      Processor: High performance MediaTek G85 Enhance gaming
-                      with 1GHz GPU 8GB of RAM including 4GB virtual 6.74 HD+
-                      90Hz display with Corning Gorilla Glass 3 Protection 50MP
-                      AI Triple camera Fast Side fingerprint 5000mAh Battery
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt amet odio nulla! Inventore aliquam sit alias quia, debitis quam aperiam, quibusdam quaerat atque et nisi laudantium dignissimos vel maiores! Vitae!
                     </li>
                     <li>
-                      Processor: High performance MediaTek G85 Enhance gaming
-                      with 1GHz GPU 8GB of RAM including 4GB virtual 6.74 HD+
-                      90Hz display with Corning Gorilla Glass 3 Protection 50MP
-                      AI Triple camera Fast Side fingerprint 5000mAh Battery
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, iure adipisci expedita alias molestias nobis ut accusantium! Sint cum, aliquam unde praesentium facere iusto, sequi error fuga inventore, eum non?
                     </li>
                   </div>
                 </div>
--- src/lib/supabase/products.tsx
+++ src/lib/supabase/products.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase =createClient(
-    process.env.SUPABASE_URL || 'https://gahojqpcesyukmwbqpyj.supabase.co',
-    process.env.SUPABASE_EON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhaG9qcXBjZXN5dWttd2JxcHlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAyNzEzNDQsImV4cCI6MjAzNTg0NzM0NH0.hDWI_fywpMvc9lqnGUnnpNAagzoOAuidP9YHthge0DU'
+    process.env.SUPABASE_URL || '',
+    process.env.SUPABASE_EON_KEY || ''
 )
